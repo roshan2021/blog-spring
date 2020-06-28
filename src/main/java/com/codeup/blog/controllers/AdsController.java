cb6e7d4f585a05ea6780bb6f0686f7d779311b00
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-06-27 22:43:38 -0500

    finish the ads controller

--- src/main/java/com/codeup/blog/controllers/AdsController.java
+++ src/main/java/com/codeup/blog/controllers/AdsController.java
@@ -2,13 +2,17 @@ package com.codeup.blog.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AdsController {
+
+    @GetMapping("/ads")
+    //@RequestMapping(value = "/ads", method = RequestMethod.GET)
+    @ResponseBody
+    public String index(){   //show list of ads = index of things
+        return "ads index page";
+    }
 
     @GetMapping("/ads/{id}")
     @ResponseBody
@@ -18,8 +22,14 @@ public class AdsController {
 
     @PostMapping("/ads/create")
     @ResponseBody
-    public String insert(){
-        return "posted";
+    public String showForm(){
+        return "View the form for creating an ad";
+    }
+
+    @PostMapping("/ads/create")
+    @ResponseBody
+    public String save(){
+        return "create a new ad";
     }
 
 }
