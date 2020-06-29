6346d39e2ed9dafc520929fbb9eca6f071acaef1
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-06-29 06:53:52 -0500

    search and findfirstByTitle functionality

--- src/main/java/com/codeup/blog/controllers/AdsController.java
+++ src/main/java/com/codeup/blog/controllers/AdsController.java
@@ -1,6 +1,6 @@
 package com.codeup.blog.controllers;
 
-
+import com.codeup.blog.daos.AdsRepository;
 import com.codeup.blog.models.Ad;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,25 +8,32 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AdsController {
 
+    // dependency injection
+    private AdsRepository adsDao;
+    public AdsController(AdsRepository adsRepository){
+        adsDao = adsRepository;
+    }
+
     @GetMapping("/ads")
-    //@RequestMapping(value = "/ads", method = RequestMethod.GET)
-    public String index(Model model){   //show list of ads = index of things
-        ArrayList<Ad> adsList = new ArrayList<>();
-        adsList.add(new Ad("PS1", "Used"));
-        adsList.add(new Ad("PS2", "Used"));
-        adsList.add(new Ad("PS4", "Used"));
-        adsList.add(new Ad("SNES", "Used"));
+//    @RequestMapping(value = "/ads", method = RequestMethod.GET)
+    public String index(Model model){
+
+        Ad firstAd = adsDao.findFirstByTitle("psvita");
+        System.out.println("firstAd.getId() = " + firstAd.getId());
+
+        List<Ad> adsList = adsDao.findAll();
         model.addAttribute("noAdsFound", adsList.size() == 0);
         model.addAttribute("ads", adsList);
         return "ads/index";
     }
 
     @GetMapping("/ads/{id}")
-    public String showAd(@PathVariable long id, Model model) {
+    public String show(@PathVariable long id, Model model){
         model.addAttribute("adId", id);
         model.addAttribute("ad", new Ad("PS1", "Used"));
         return "/ads/show";
@@ -40,8 +47,49 @@ public class AdsController {
 
     @PostMapping("/ads/create")
     @ResponseBody
-    public String save(){
+    public String save()
+    {
+        Ad newAd = new Ad("XBOX X","brand new");
+        adsDao.save(newAd);
         return "create a new ad";
     }
+
+    @GetMapping("/ads/{id}/edit")
+    public String showEditForm(Model model, @PathVariable long id){
+        // find an ad
+        Ad adToEdit = adsDao.getOne(id);
+        model.addAttribute("ad", adToEdit);
+        return "ads/edit";
+    }
+
+    @PostMapping("/ads/{id}/edit")
+    @ResponseBody
+    public String update(@PathVariable long id,
+                         @RequestParam(name = "title") String title,
+                         @RequestParam(name = "description") String desc){
+        // find an ad
+        Ad foundAd = adsDao.getOne(id); // select * from ads where id = ?
+        // edit the ad
+        foundAd.setTitle(title);
+        foundAd.setDescription(desc);
+        // save the changes
+        adsDao.save(foundAd); // update ads set title = ? where id = ?
+        return "ad updated";
+    }
+
+    @PostMapping("/ads/{id}/delete")
+    @ResponseBody
+    public String destroy(@PathVariable long id){
+        adsDao.deleteById(id);
+        return "ad deleted";
+    }
+
+    @GetMapping("/search")
+    public String searchResults(Model model, @RequestParam(name = "term") String term){
+        List<Ad> ads = adsDao.searchByTitle(term);
+        model.addAttribute("ads", ads);
+        return "ads/index";
+    }
+
 
 }
