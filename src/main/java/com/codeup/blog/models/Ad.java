4674aeab81911db3873be1b403d4cbc14dfa55dd
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-06-28 22:58:39 -0500

    feat: add jpa annotations to make this java class into a db table

--- src/main/java/com/codeup/blog/models/Ad.java
+++ src/main/java/com/codeup/blog/models/Ad.java
@@ -1,10 +1,36 @@
 package com.codeup.blog.models;
 
+import javax.persistence.*;
+
+@Entity
+//Table is a representation of many ads
+@Table(name = "ads")
+//representation of ONE Ad
 public class Ad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 200)
     private String title;
+
+    @Column(nullable = false)
     private String description;
 
+    //Spring Framework uses this empty constructor
+    public Ad() {
+    }
+
+    //insert Ad
     public Ad(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    //read Ad
+    public Ad(long id, String title, String description) {
+        this.id = id;
         this.title = title;
         this.description = description;
     }
@@ -23,5 +49,13 @@ public class Ad {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
